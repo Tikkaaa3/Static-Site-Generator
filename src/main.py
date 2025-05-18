54a39fb306d2aac4ac733fbc5a4c9a198ab244ce
Tikkaaa3 <tikkaaa3@gmail.com>
2025-05-18 23:13:31 +0200

    github pages update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+import sys
+
 from page import copy_content, generate_pages_recursive
 
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     copy_content()
-    generate_pages_recursive("content/", "template.html", "public/")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 
 main()
--- src/page.py
+++ src/page.py
@@ -4,7 +4,7 @@ import shutil
 from markdown_to_html import markdown_to_html_node
 
 
-def copy_content(src="static/", dest="public/", is_root=True):
+def copy_content(src="./static", dest="./docs", is_root=True):
     if os.path.exists(dest) and is_root:
         shutil.rmtree(dest)
 
@@ -28,31 +28,33 @@ def extract_title(markdown):
     raise Exception(f"No title found in {markdown}")
 
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using template_path")
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     f = open(from_path)
     markdown_file = f.read()
     f.close()
 
     f = open(template_path)
-    template_file = f.read()
+    template = f.read()
     f.close()
 
     node = markdown_to_html_node(markdown_file)
-    node = node.to_html()
+    html = node.to_html()
     title = extract_title(markdown_file)
 
-    result = template_file.replace("{{ Title }}", title)
-    result = result.replace("{{ Content }}", node)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as f:
-        f.write(result)
+        f.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         content_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
@@ -60,8 +62,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             content_path,
         ) and content_path.endswith(".md"):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(content_path, template_path, dest_path)
+            generate_page(content_path, template_path, dest_path, basepath)
         elif os.path.isfile(content_path):
             continue
         else:
-            generate_pages_recursive(content_path, template_path, dest_path)
+            generate_pages_recursive(content_path, template_path, dest_path, basepath)
